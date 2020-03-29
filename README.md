61b50f79dd0f99cc465c5af28aeeb9f313360b0e
E. Fynn <me@enriquefynn.com>
2020-03-29 11:48:35 +0200

    update in downloader

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 #Math PhD Graph
 
-With this you can get the full ancestry of the Mathematics academia in a visually appealing graph, hosted on [phdmathgraph.com](http://phdmathgraph.com)
+With this you can get the full ancestry of the Mathematics academia in a visually appealing graph
 
-You can also just use it's methods like a library.
+You can also just use its methods like a library.
 
-To run it just type `npm install` and serve the static files in client
+To re-download all graph do `npm install` and run the download_all.js script, it outputs a csv with all the database from the Mathematics Genealogy Project.
 
 This is built using [Cytoscape](http://js.cytoscape.org/) and with the database gathered from the [Mathematics Genealogy Project](https://www.genealogy.math.ndsu.nodak.edu/index.php)
 
-
+## Disclosure
+I sent an email to the prof. owner of the Mathematics Genealogy Project which advised me to cease completely the usage of this program.
+This database doesn't belong to a single so-called scientist.
--- download_all.js
+++ download_all.js
@@ -1,59 +1,51 @@
 'use strict';
 const http_methods = require('./http_methods');
 const promise = require('bluebird');
-const redis = require('redis');
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('names.sql');
-//db.run('DROP TABLE USER');
-db.run('CREATE TABLE IF NOT EXISTS USER (Name TEXT, Id INTEGER PRIMARY KEY, Faculty TEXT, Year TEXT)');
-db.run('CREATE INDEX IF NOT EXISTS name_idx ON USER(Name)');
 
-var cache = redis.createClient();
+// var cache = redis.createClient();
 promise.promisifyAll(redis.RedisClient.prototype);
-cache.on('error', console.error);
+// cache.on('error', console.error);
 
-var max = 206701;
+var max = 255426;
 var idx = 1;
-function getAll(){
-    try
-    {
-        return http_methods.getInfo(idx).then(data => {
-            console.log(data);
-            cache.set(data.id, JSON.stringify({
-               name: data.name,
-               faculty: data.faculty,
-               year: data.year,
-               advisors: data.advisors.map(el => el.id)
-            }));
-        }).then(() => {
-            if (idx < max)
-            {
-                idx = idx+1;
-                getAll();
-            }
-        }).catch((err) => {
-            if (err.code !== undefined)
-            {
-                console.error('Connection error', err);
-                getAll();
-            }
-            else
-            {
-                console.error('Probably not a valid index', idx, err);
-                if (idx < max)
-                {
-                    idx = idx+1;
-                    getAll();
-                }
-            }
-        });
-    }
-    catch(err)
-    {
-        console.error('Ops', err);
+
+function getAll() {
+  try {
+    return http_methods.getInfo(idx).then(data => {
+      let advisors = data.advisors.map(el => el.id).toString()
+
+      let d = `${data.id}, ${data.name}, ${data.faculty}, ${data.year}, ${advisors}`
+      console.log(d);
+    }).then(() => {
+      if (idx < max) {
+        idx = idx + 1;
         getAll();
-    }
+      }
+    }).catch((err) => {
+      if (err.code !== undefined) {
+        console.error('Connection error', err);
+        getAll();
+      }
+      else {
+        console.error('Probably not a valid index', idx, err);
+        if (idx < max) {
+          idx = idx + 1;
+          getAll();
+        }
+      }
+    });
+  }
+  catch (err) {
+    console.error('Ops', err);
+    getAll();
+  }
 }
+
+if (process.argv.length > 1) {
+  min = parseInt(process.argv[1])
+  max = parseInt(process.argv[2])
+}
+
 getAll();
 
 /*
